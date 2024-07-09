2cbedb86583bcad4bbc730b81969e3fe224a3452
ShinHeeJin <sisesu92@gmail.com>
2024-07-09 23:25:01 +0900

    ✨ StyleWidgets

---
+++ tutorial/9. styling_widgets.py
@@ -0,0 +1,23 @@
+import sys
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QLabel
+
+app = QApplication([])
+w = QLabel("This is a placeholder text")
+# w.setAlignment(Qt.AlignmentFlag.AlignCenter)
+# w.setStyleSheet(
+#     """
+#         background-color: #262626;
+#         color: #FFFFFF;
+#         font-family: Titillium;
+#         font-size: 18px;
+#     """
+# )
+w.show()
+
+with open("../ui/style.qss", "r") as f:
+    _style = f.read()
+    app.setStyleSheet(_style)
+
+sys.exit(app.exec())
